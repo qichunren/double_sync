fbe7f9b6ab59c2f7f3c3ae8326b5b2f0dc5e9d41
qichunren <whyruby@gmail.com>
2022-01-20 22:17:48 +0800

    Hold global pointer_clone instance.

--- content.js
+++ content.js
@@ -1,35 +1,34 @@
-let img = document.createElement("img");
-img.id = "double_sync_pointer";
-img.src = chrome.runtime.getURL("./images/arrow-pointer.png");
-img.style.position = "fixed";
-img.style.zIndex = 99999;
-img.style.left = "10px";
-img.style.top = "10px";
-img.style.display = "none";
-document.body.appendChild(img);
+var pointer_clone = document.createElement("img");
+pointer_clone.id = "double_sync_pointer";
+pointer_clone.src = chrome.runtime.getURL("./images/arrow-pointer.png");
+pointer_clone.style.position = "fixed";
+pointer_clone.style.zIndex = 99999;
+pointer_clone.style.left = "10px";
+pointer_clone.style.top = "10px";
+pointer_clone.style.display = "none";
+document.body.appendChild(pointer_clone);
 
 window.addEventListener('scroll', function(e) {  
-  if(img.style.display == "none") {
+  if(pointer_clone.style.display == "none") {
     chrome.runtime.sendMessage({event: "scroll", url: window.location.href, scrolly: window.scrollY});
   }
 });
 
 window.addEventListener('mousemove', function(e) {
-  img.style.display = "none";
+  pointer_clone.style.display = "none";
   chrome.runtime.sendMessage({event: "mousemove", url: window.location.href, client_x: e.clientX, client_y: e.clientY});
 });
   
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-      console.log("RECV notify", request);      
-      var pointer = document.getElementById("double_sync_pointer");
-      pointer.style.display = "block";
+      console.log("RECV notify", request);            
+      pointer_clone.style.display = "block";
       if(request.event == "scroll") {
         window.scrollTo(0, request.scrolly);
       } else if(request.event == "mousemove") {                
-        pointer.style.left = "" + request.client_x + "px";
-        pointer.style.top = "" + request.client_y + "px";
+        pointer_clone.style.left = "" + request.client_x + "px";
+        pointer_clone.style.top = "" + request.client_y + "px";
         console.log("Force set pointer positoin");
       }
 
